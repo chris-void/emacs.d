035d5372794a2f59414a61b6d80a402b8d3b8c7d
shwsun_ <shwsun@csa2.bu.edu>
2015-10-01 16:56:37 -0400

    update

---
+++ elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (22029 38938 823398
+;;;;;;  75000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
---
+++ elpa/flycheck-pyflakes-20140630.1521/flycheck-pyflakes-autoloads.el
@@ -0,0 +1,18 @@
+;;; flycheck-pyflakes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("flycheck-pyflakes-pkg.el" "flycheck-pyflakes.el")
+;;;;;;  (22029 39053 439392 849000))
+
+;;;***
+
+(provide 'flycheck-pyflakes-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-pyflakes-autoloads.el ends here
