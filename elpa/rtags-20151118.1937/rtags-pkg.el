84d0a3b073060b7f95d28b980968d56bbd8465a0
shwsun <shwsun@bu.edu>
2015-11-29 17:00:12 -0500

    update

---
+++ elpa/rtags-20151118.1937/rtags-pkg.el
@@ -0,0 +1,4 @@
+(define-package "rtags" "20151118.1937" "A front-end for rtags" 'nil :url "http://rtags.net")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/spacegray-theme-20150719.1231/spacegray-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; spacegray-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (22100
+;;;;;;  41578 0 0))
+;;; Generated autoloads from spacegray-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("spacegray-theme-pkg.el") (22100 41578
+;;;;;;  396874 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; spacegray-theme-autoloads.el ends here
